2b7d3c63ed9b049b8079f793a50760c404640914
Joseph Bradley <josephbradley16@googlemail.com>
2021-01-09 16:06:44 +0000

    Adding quick R test to make sure gapminder is installed

--- test_setup.R
+++ test_setup.R
@@ -2,3 +2,8 @@ library(shiny)
 library(tidyverse)
 library(devtools)
 library(WDI)
+
+gapm <- gapminder::gapminder %>%
+  filter(country %in% g7) %>%
+  select(country, year, gdpPercap, lifeExp) %>%
+  group_by(country)
